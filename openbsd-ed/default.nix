215ebc0b85c8d7e6d94d23ac908c08220cc3be79
Ypnose <hmocode@exo.re>
2023-10-14 08:07:31 +0200

    Add openbsd-ed

---
+++ openbsd-ed/default.nix
@@ -0,0 +1,28 @@
+{ lib
+, stdenv
+, fetchFromGitHub
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "openbsd-ed";
+  version = "0.5";
+
+  src = fetchFromGitHub {
+    owner = "Ypnose";
+    repo = "openbsd-ed";
+    rev = "7a3fc10df2d1250de3b732cc033d245fdcbe7f47";
+    sha256 = "sha256-eS17jgwSmpfRd1Rqk5x0bsHqVzuzVE5NFfKEoSPeXfM=";
+  };
+
+  dontConfigure = true;
+
+  installPhase = ''
+    install -Dm 0755 ed $out/bin/ed
+  '';
+
+  meta = {
+    homepage = "https://www.openbsd.org/";
+    description = "Line-oriented text editor";
+    platforms = lib.platforms.unix;
+  };
+})
