be06d7d40f205957eb86b85fc628d37a26a71544
Ypnose <hmocode@exo.re>
2024-11-23 22:05:27 +0100

    go-pushover: change repo source location

--- go-pushover/default.nix
+++ go-pushover/default.nix
@@ -4,23 +4,25 @@
 , buildGoModule
 }:
 
-buildGoModule {
+buildGoModule rec {
   pname = "go-pushover";
-  version = "20241121";
+  version = "20241123";
 
   src = fetchFromGitHub {
     owner = "ypnose";
-    repo = "go-pushover";
-    rev = "3c0c6b12750bed87d3fd7d3821a3f8dee80e3e86";
-    hash = "sha256-fAJ2Mw5naHXeyRPXVgx0IlYM3Y6fwCcso9415tBMoio=";
+    repo = "gools";
+    rev = "a41d8aa76ce769b7aa045590ec93ffc0c954c9f3";
+    hash = "sha256-dYwRh5bx5XelcS6mqy4Gmw5Di8uzjxufXyFf0U1Ne+s=";
   };
 
   vendorHash = null;
 
+  sourceRoot = "${src.name}/go-pushover";
+
   ldflags = [ "-s" ];
 
   meta = {
-    homepage = "https://github.com/Ypnose/go-pushover";
+    homepage = "https://github.com/Ypnose/gools";
     description = "Simple tool to send data to Pushover Message API";
     platforms = lib.platforms.unix;
   };
