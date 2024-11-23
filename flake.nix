4f51c54f464bc7b38f0c64b466eaec21898d5639
Ypnose <hmocode@exo.re>
2024-11-23 22:04:38 +0100

    flake.nix: improve and support other architectures

--- flake.nix
+++ flake.nix
@@ -1,4 +1,6 @@
 {
+  description = "Collection of various packages and tools";
+
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
@@ -9,19 +11,26 @@
   # nix log .#PKG
   outputs = { self, nixpkgs, ... }:
     let
-      system = "x86_64-linux";
-      cmd = nixpkgs.legacyPackages.${system};
-      build = pkg: cmd.callPackage ./${pkg} {};
-      build-static = pkg: cmd.pkgsStatic.callPackage ./${pkg} {};
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+
+      mkPackages = system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          build = pkg: pkgs.callPackage ./${pkg} {};
+          build-static = pkg: pkgs.pkgsStatic.callPackage ./${pkg} {};
+        in {
+          apacheds = build "apacheds";
+          dig-only = build "dig-only";
+          go-pushover = build "go-pushover";
+          nvi-patched = build "nvi-patched";
+          openbsd-ed = build-static "openbsd-ed";
+          s-nail = build "s-nail";
+          wg = build-static "wg";
+          wifycon = build "wifycon";
+          yss = build "yss";
+        };
     in {
-      packages.x86_64-linux.apacheds = build "apacheds";
-      packages.x86_64-linux.dig-only = build "dig-only";
-      packages.x86_64-linux.go-pushover = build "go-pushover";
-      packages.x86_64-linux.nvi-patched = build "nvi-patched";
-      packages.x86_64-linux.openbsd-ed = build-static "openbsd-ed";
-      packages.x86_64-linux.s-nail = build "s-nail";
-      packages.x86_64-linux.wg = build-static "wg";
-      packages.x86_64-linux.wifycon = build "wifycon";
-      packages.x86_64-linux.yss = build "yss";
-  };
+      packages = forAllSystems mkPackages;
+    };
 }
