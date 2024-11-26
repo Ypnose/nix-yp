fa5dde7d6b89d232769857c8c560d3578ac9e254
Ypnose <hmocode@exo.re>
2024-11-26 10:21:44 +0100

    csvview: update to latest commit

--- csvview/default.nix
+++ csvview/default.nix
@@ -11,8 +11,8 @@ buildGoModule rec {
   src = fetchFromGitHub {
     owner = "ypnose";
     repo = "gools";
-    rev = "fee01f6dfac1eb180f769c137f82cf15fa1f80ff";
-    hash = "sha256-m5lF77OsjK7TT2mbcXiJD8VD1cTOziLF+yOTCVYqzLQ=";
+    rev = "a7843a052e3f095b425060209eeea4d111f84cf1";
+    hash = "sha256-5VZgq3FYiclGNcxEWWmQ8PlYbVKxbGaIAm/DWuJpmJo=";
   };
 
   vendorHash = "sha256-m/ASIIuV5W/KNw1YBXrQTLiTSdHA7R3ir5+Ivb3W6Ns=";
