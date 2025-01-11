a45e74c3a2b15161b2828c04db967e76db158a64
Ypnose <hmocode@exo.re>
2025-01-11 11:48:42 +0100

    ipinfo: update to latest commit

--- ipinfo/default.nix
+++ ipinfo/default.nix
@@ -6,20 +6,20 @@
 
 buildGoModule rec {
   pname = "ipinfo";
-  version = "20241207";
+  version = "20250111";
 
   src = fetchFromGitHub {
     owner = "ypnose";
     repo = "gools";
-    rev = "f73d9b16c6dcb98c583ae9eef83e883d7c097472";
-    hash = "sha256-zhZLqUGKTGL9SHxr4Ci1VJ5/Vcnm7aN9LSNr6ALH45E=";
+    rev = "24375a7d3e0d1c928e04c1a592bf684fef311b36";
+    hash = "sha256-Y4zjoQiTzSQKwW2vdfeF+7SRAp7Rwyx4RsUt4LXFq0U=";
   };
 
   vendorHash = null;
 
   sourceRoot = "${src.name}/ipinfo";
 
-  ldflags = [ "-s" ];
+  ldflags = [ "-w -s" ];
 
   meta = {
     homepage = "https://github.com/Ypnose/gools";
