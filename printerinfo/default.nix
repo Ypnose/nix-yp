8b2edfab976e254537d79f25ab2d1edc53130093
Ypnose <hmocode@exo.re>
2025-07-17 09:53:37 +0200

    printerinfo: update to latest commit

--- printerinfo/default.nix
+++ printerinfo/default.nix
@@ -6,16 +6,16 @@
 
 buildGoModule rec {
   pname = "printerinfo";
-  version = "20250502";
+  version = "20250717";
 
   src = fetchFromGitHub {
     owner = "ypnose";
     repo = "gools";
-    rev = "9d5dd83abf65c5c06b88dcac4cd9f245944630c2";
-    hash = "sha256-yiumfIClrlPbZImv8EpreCMoCcEd5Zt2xf/0TbgXgOA=";
+    rev = "dce4bbcb3beaa09d4661e9479e3b19ce02c1e04e";
+    hash = "sha256-mMA9Unb0VeKfxtDGEh19cvlE6SJ1Ac85E9C6jmggne0=";
   };
 
-  vendorHash = "sha256-LC80ez4cnZ+ssBdoNe3OSnvc5SSrq+1wJDvBUjtIcqc=";
+  vendorHash = "sha256-X1nod3uwyazlVFRPJjLP0iLjbCKWX9LIMgRnpfqJl5Y=";
 
   sourceRoot = "${src.name}/printerinfo";
 
