4f087a9aa34b6623b99d4968ac5f95c225c5eba9
Ypnose <hmocode@exo.re>
2026-02-21 19:53:26 +0100

    vipe: update to latest commit

--- vipe/default.nix
+++ vipe/default.nix
@@ -6,13 +6,13 @@
 
 buildGoModule rec {
   pname = "vipe";
-  version = "20260205";
+  version = "20260221";
 
   src = fetchFromGitHub {
     owner = "ypnose";
     repo = "gools";
-    rev = "08cee83aed61e7a95ada08a36fc8b469bdc4baf3";
-    hash = "sha256-cYiHYSr7B7DxSpgb4yBpSbJkM0BcnmiZ0YfcyrKjQAw=";
+    rev = "b66a74c7f32b0fc94cb5868b154c1d0dab893de1";
+    hash = "sha256-5yrMzQUxwKD+3VJpDRp3abK3AAFWdviwXpYtAuRFAGo=";
   };
 
   vendorHash = null;
