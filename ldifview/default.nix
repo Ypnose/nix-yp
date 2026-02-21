442d6d8da7f95f30c3b9231f31394f09bdb0aa69
Ypnose <hmocode@exo.re>
2026-02-21 19:52:59 +0100

    ldifview: update to latest commit

--- ldifview/default.nix
+++ ldifview/default.nix
@@ -6,13 +6,13 @@
 
 buildGoModule rec {
   pname = "ldifview";
-  version = "20250930";
+  version = "20260221";
 
   src = fetchFromGitHub {
     owner = "ypnose";
     repo = "gools";
-    rev = "89af542b73e88568d8e26b23b6817373c2c74482";
-    hash = "sha256-kkoYJbdy5QNPWqP60RLLHyzZVVD22CcHrWfODxBj/IQ=";
+    rev = "b66a74c7f32b0fc94cb5868b154c1d0dab893de1";
+    hash = "sha256-5yrMzQUxwKD+3VJpDRp3abK3AAFWdviwXpYtAuRFAGo=";
   };
 
   vendorHash = null;
