8dd312438cc491fb7b88a8e4d44478c09ede9f4c
Ypnose <hmocode@exo.re>
2026-05-23 14:17:01 +0200

    csvview: update to latest commit

--- csvview/default.nix
+++ csvview/default.nix
@@ -5,16 +5,16 @@
 
 buildGoModule rec {
   pname = "csvview";
-  version = "20260221";
+  version = "20260523";
 
   src = fetchFromGitHub {
     owner = "ypnose";
     repo = "gools";
-    rev = "b66a74c7f32b0fc94cb5868b154c1d0dab893de1";
-    hash = "sha256-5yrMzQUxwKD+3VJpDRp3abK3AAFWdviwXpYtAuRFAGo=";
+    rev = "1c1cbe2f3f36e31b5872c99473d8775e3b768462";
+    hash = "sha256-3xCKZHYeNtO+ur2Fti6OBr1mZQC3Tcgn3ycVfz3JsII=";
   };
 
-  vendorHash = "sha256-ew4OejtBoLuAnT2GbQWKSbvO9efLw/BUKfhSmX4y7c0=";
+  vendorHash = "sha256-cuIxIlp4b/X5n2cFUlGdccI1r/LJrxiVY1bhaaUSuyA=";
 
   sourceRoot = "${src.name}/csvview";
 
